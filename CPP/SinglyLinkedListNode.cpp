b0442acc49ab4915482aa2bda80792d0a3aff1a0
Dipanshuofficial <114001896+Dipanshuofficial@users.noreply.github.com>
2023-01-08 20:04:44 +0530

    Create SinglyLinkedListNode.cpp

---
+++ CPP/SinglyLinkedListNode.cpp
@@ -0,0 +1,31 @@
+
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    SinglyLinkedListNode* neww=new SinglyLinkedListNode(data);
+    SinglyLinkedListNode* temp;
+    temp=head;
+    if(head==NULL){
+        head=neww;
+    }
+    else{
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=neww;
+        neww->next=NULL;
+    }
+    return head;
+
+}
+
